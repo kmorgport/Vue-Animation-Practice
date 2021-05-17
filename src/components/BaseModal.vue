e9610db7067f43d034bb9ac72d4a4f6b44e84221
kmorgport <kmorgan0057@gmail.com>
2021-05-16 22:16:33 -0500

    custom modal animations vue css

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
   <transition name="modal">
-  <dialog open>
+  <dialog open v-if="open">
     <slot></slot>
   </dialog>
   </transition>
@@ -9,6 +9,7 @@
 
 <script>
 export default {
+  props: ['open'],
   emits: ['close'],
 };
 </script>
@@ -36,11 +37,16 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
-  animation: modal 0.3s ease-out forwards;
+  /* animation: modal 0.3s ease-out forwards; */
 }
 
 .modal-enter-active{
-  animation: modal 0.3 ease-out;
+  animation: modal 0.3s ease-out;
+}
+
+
+.modal-leave-active{
+  animation: modal 0.3s ease-in reverse;
 }
 
 @keyframes modal{
